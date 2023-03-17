bb13b1d05e375241288391942657134679fe5c22
ch3436 <connh99@gmail.com>
2023-03-17 04:52:00 +0000

    Update LeagueOfLegendsReader.py

--- public/LeagueOfLegendsReader.py
+++ public/LeagueOfLegendsReader.py
@@ -1,104 +1,36 @@
 import requests
+import json
 
-def requestSummonerData(region, summonerName, APIKey):
-    URL = "https://" + region + ".api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerName + "?api_key=" + APIKey
-    response = requests.get(URL)
-    return response.json()
+# get summoner ID
+summoner_name = input("Enter summoner name: ")
+api_key = "RGAPI-c693b3ed-8b35-4418-92ad-6fa9189a650e"
+region = input("Region: ")
+url = "https://" + region + ".api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summoner_name + "?api_key=" + api_key
+response = requests.get(url)
+summoner_info = json.loads(response.content)
+summoner_id = summoner_info["id"]
 
-def requestRankedData(region, ID, APIKey):
-    URL = "https://" + region + ".api.riotgames.com/lol/league/v4/entries/by-summoner/" + ID + "?api_key=" + APIKey
-    response = requests.get(URL)
-    return response.json()
+# get champion mastery data
+url = "https://" + region + ".api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/" + summoner_id + "?api_key=" + api_key
+response = requests.get(url)
+champ_mastery_list = json.loads(response.content)
 
-def requestChampLists(region, AID, CID, APIKey):
-    URL = "https://" + region + ".api.riotgames.com/lol/match/v4/matchlists/by-account/" + AID + "?beginIndex=99999" + "&champion=" + CID + "&api_key=" + APIKey
-    response = requests.get(URL)
-    return response.json()
+# get summoner rank data
+url = "https://" + region + ".api.riotgames.com/lol/league/v4/entries/by-summoner/" + summoner_id + "?api_key=" + api_key
+response = requests.get(url)
+rank_info = json.loads(response.content)
 
-def requestMatchLists(region, AID, APIKey):
-    URL = "https://" + region + ".api.riotgames.com/lol/match/v4/matchlists/by-account/" + AID + "?beginIndex=99999" + "&api_key=" + APIKey
-    response = requests.get(URL)
-    return response.json()
-
-def requestChampMastery(region, ID, CID, APIKey):
-    URL = "https://" + region + ".api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/" + ID + "/by-champion/" + CID + "?api_key=" + APIKey
-    response = requests.get(URL)
-    return response.json()
-
-def requestChampWinrate(region, AID, CID, APIKey):
-    URL = "https://" + region + ".api.riotgames.com/lol/match/v4/matchlists/by-account/" + AID + "?endIndex=50" + "&champion=" + CID + "&queue=420" + "&api_key=" + APIKey
-    response = requests.get(URL)
-    return response.json()
-
-def RequestChampMatch(region, gameID, APIKey):
-    URL = "https://" + region + ".api.riotgames.com/lol/match/v4/matches/" + gameID + "?api_key=" + APIKey
-    response = requests.get(URL)
-    return response.json()
-
-def winrate(winloss: list):
-    return winloss[0] / (winloss[0] + winloss[1]) * 100
-
-
-region = (str)(input('Region: '))
-summonerName = (str)(input('Summoner Name: '))
-CID = (str)(input('Champion ID: '))
-APIKey = "RGAPI-c693b3ed-8b35-4418-92ad-6fa9189a650e"
-win_loss = [0,0]
-
-responseJSON  = requestSummonerData(region, summonerName, APIKey)
-try:
-    ID = str(responseJSON['id'])
-    AID = str(responseJSON['accountId'])
-except KeyError:
-    print('Summoner not found!')
-    exit()
-
-responseJSON2 = requestRankedData(region, ID, APIKey)
-if responseJSON2:
-    print("\nTIER:", (responseJSON2[0]['tier']))
-    print("RANK:", (responseJSON2[0]['rank']))
-    print("LP:", (responseJSON2[0]['leaguePoints']))
-else:
-    print("No ranked data found.")
-
-playtime_seconds = responseJSON['revisionDate']
-playtime_hours = playtime_seconds / 3600
-print(f"Total playtime in hours: {playtime_hours:.2f}")
-
-responseJSON3 = requestChampLists(region, AID, CID, APIKey)
-try:
-    otGames = int(responseJSON3['totalGames'])
-except KeyError:
-    print('No games found for this champion!')
-    exit()
-responseJSON4 = requestMatchLists(region, AID, APIKey)
-total = int(responseJSON4['totalGames'])
-
-if otGames / total >= 0.45:
-    print("OTP: YES")
-else:
-    print("OTP: NO")
-    
-responseJSON5 = requestChampMastery(region, ID, CID, APIKey)
-print("MASTERY POINTS:", (responseJSON5['championPoints']))
-
-matchList = requestChampWinrate(region, AID, CID, APIKey)
-
-for match in matchList['matches']:
-    GID = str(match['gameId'])
-    matchInfo = RequestChampMatch(region, GID, APIKey)
-    participantId = 0
-    for player in matchInfo['participantIdentities']:
-        if player['player']['summonerName'] == summonerName:
-            participantId = player['participantId']
-    if participantId <= 5:
-        participantId = 0
-    else:
-        participantId = 1
-    if matchInfo['teams'][participantId]['win'] == 'Win':
-        win_loss[0] += 1
-    else:
-        win_loss[1] += 1
-print('\n')
-print(win_loss[0], 'WINS', win_loss[1], 'LOSSES')
-print("RATING: " + ('%.2f'%(winrate(win_loss)) + "%"))
+# create  dicionary with playtime hours and rank information
+statDict = {}
+if rank_info:
+    statDict["playtime_hours"] = round(sum([int(champ_mastery['championPoints']) for champ_mastery in champ_mastery_list])/60, 2)
+    statDict["rank_information"] = []
+    for rank in rank_info:
+        queue_type = rank["queueType"]
+        tier = rank["tier"]
+        rank = rank["rank"]
+        lp = str(rank_info[0]["leaguePoints"]) + " LP"
+        rank_info_str = queue_type + " - " + tier + " " + rank + " (" + lp + ")"
+        statDict["rank_information"].append(rank_info_str)
+        
+print(statDict)
