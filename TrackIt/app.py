0a3c1ec5bae182a4a40608bce7dbe262913daf8a
jj3249 <jj3249@drexel.edu>
2023-05-05 11:30:16 -0400

    Latest version of TrackIt

--- TrackIt/app.py
+++ TrackIt/app.py
@@ -4,7 +4,6 @@ from roblox import roblox
 from Mojang import Mojang
 from Steam import Steam
 from LeagueOfLegends import LeagueOfLegends
-from Apex import Apex
 
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/")
@@ -12,7 +11,6 @@ app.register_blueprint(roblox, url_prefix="/")
 app.register_blueprint(Mojang, url_prefix="/")
 app.register_blueprint(Steam, url_prefix="/")
 app.register_blueprint(LeagueOfLegends, url_prefix="/")
-app.register_blueprint(Apex, url_prefix="/")
 
 if __name__=='__main__':
     app.run(debug=True)
