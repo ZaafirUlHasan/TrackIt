4d5b9db3774a3a3517e7592caa6d526be2ae8b6d
jj3249 <jj3249@drexel.edu>
2023-05-29 18:05:56 -0400

    Implemented Overwatch API

--- TrackIt/Mojang.py
+++ TrackIt/Mojang.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, current_app
 import requests
+from datetime import datetime
 from mojang import Client
 
 Mojang = Blueprint(__name__, "Mojang")
@@ -20,21 +21,22 @@ def getMojangStats():
     if request.method == 'POST':
         return redirect(url_for('Mojang.getMojangID'))
     try:
-        #Placeholder, it is my minecraft account, user input will be needed.
-        user_id = request.args.get('user_id', 'evanpalubinski@gmail.com')
-        password = request.args.get('password', 'Binski55')
-        user_name= request.args.get('user_name', 'DJSALADD')
-        client = Client(user_id, password)
-        #Getting user inputs
+    #Placeholder, it is my minecraft account, user input will be needed.
         user_id = request.args.get('user_id')
         password = request.args.get('password')
         user_name= request.args.get('user_name')
+        client = Client(user_id, password)
+        #Getting user inputs
         #Getting the NameInformation class object
         client = Client(user_id, password)
         name_change_info = client.get_name_change_info()
         #Extract the creation date from the name_change_info dictionary
-        creation_date = name_change_info.created_at.strftime("%d-%m-%y")
-        creation_format = "The Minecraft account was created on "+creation_date
+        created_at_str = name_change_info.created_at 
+        created_at_datetime = datetime.strptime(created_at_str, "%Y-%m-%dT%H:%M:%SZ")
+        creation_date = created_at_datetime.strftime("%d-%m-%y")
+        creation_format = "The Minecraft account was created on " + creation_date
+
+
         uuid_response = requests.get(f'https://api.mojang.com/users/profiles/minecraft/{user_name}')
         uuid = uuid_response.json()['id']
         skin = (f'https://crafatar.com/skins/{uuid}')
@@ -42,6 +44,6 @@ def getMojangStats():
         body = (f'https://crafatar.com/renders/body/{uuid}')
         avatar = (f'https://crafatar.com/avatars/{uuid}')
         return render_template("myMojangPage.html", date=creation_format,skin=skin, cape=cape, body=body, avatar=avatar,
-                               skin_heading="Skin",cape_heading="Cape", body_heading="Body", avatar_heading="Avatar")
+                                skin_heading="Skin",cape_heading="Cape", body_heading="Body", avatar_heading="Avatar")
     except:
         return render_template("Mojang.html")
